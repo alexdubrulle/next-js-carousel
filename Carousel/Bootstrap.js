7feb062034f840dab16eeb700c6565f24e22d5d4
alexdubrulle <alexdubrullefagnoni26@hotmail.fr>
2023-09-14 10:13:05 +0200

    carousel erreur css import bootstrap

---
+++ Carousel/Bootstrap.js
@@ -0,0 +1,28 @@
+"use client";
+import { useState } from "react";
+import { items } from "../public/Items.json";
+import { Carousel } from "react-bootstrap";
+import styles from "../styles/Bootstrap.module.css";
+import "bootstrap/dist/css/bootstrap.min.css"; // Mettez cet import en dernier
+
+export default function BootstrapCarousel() {
+  const { bootstrap } = items;
+  const [index, setIndex] = useState(0);
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      {bootstrap.map((item) => (
+        <Carousel.Item key={item.id} className={styles.itemP} interval={4000}>
+          <img src={item.imageUrl} alt="slides" />
+          <Carousel.Caption className={styles.caption}>
+            <h3>{item.title}</h3>
+            <p>{item.body}</p>
+            <button className="btn btn-danger">Visit Docs</button>
+          </Carousel.Caption>
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
+}
